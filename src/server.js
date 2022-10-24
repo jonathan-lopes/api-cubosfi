9bd98687c59bd4d4aeb2d8c6c95a24f666d7a1ba
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-10-24 18:52:07 -0300

    feat: register error middleware and import express-async-errors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
+require('express-async-errors');
 const express = require('express');
 const routes = require('./routes/router');
 const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
 const helmet = require('helmet');
+const errorMiddleware = require('./middleware/error');
 
 const app = express();
 
@@ -14,5 +16,6 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(routes);
+app.use(errorMiddleware);
 
 module.exports = app;
