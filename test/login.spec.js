1c5984b560026c7956b425ac3a4aae93a60f2f0e
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-10-24 23:00:07 -0300

    test: add tests for login endpoint

---
+++ test/login.spec.js
@@ -0,0 +1,89 @@
+const request = require('supertest');
+const app = require('../src/server');
+const knex = require('../src/database/connection');
+const bcrypt = require('bcrypt');
+
+const createSut = async () => {
+  const user = {
+    name: 'testman',
+    email: 'testman@email.com',
+    password: await bcrypt.hash('testman1234', +process.env.SALT_ROUNDS),
+  };
+
+  await knex('users').insert(user);
+
+  return { name: user.name, email: user.email, password: 'testman1234' };
+};
+
+const clear = async () => {
+  await knex('users').del().where({ email: 'testman@email.com' });
+};
+
+describe('Enpoint login', () => {
+  afterEach(async () => await clear());
+
+  it('should fail if not send body email and password', async () => {
+    const { email, password } = await createSut();
+
+    const responseEmail = await request(app).post('/login').send({
+      email,
+    });
+
+    expect(responseEmail.status).toBe(400);
+    expect(responseEmail.body).toHaveProperty(
+      'message',
+      'password é um campo obrigatório',
+    );
+
+    const responsePasswd = await request(app).post('/login').send({
+      password,
+    });
+
+    expect(responsePasswd.status).toBe(400);
+    expect(responsePasswd.body).toHaveProperty(
+      'message',
+      'email é um campo obrigatório',
+    );
+  });
+
+  it('should fail if email not exist', async () => {
+    const response = await request(app).post('/login').send({
+      email: 'jonhTest@email.com',
+      password: 'jonh1234',
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty(
+      'message',
+      'E-mail ou senha inválidos',
+    );
+  });
+
+  it('should fail if password is incorrect', async () => {
+    const { email } = await createSut();
+
+    const response = await request(app).post('/login').send({
+      email,
+      password: 'test123456',
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty(
+      'message',
+      'E-mail ou senha inválidos',
+    );
+  });
+
+  it('should returning user with token', async () => {
+    const { email, password } = await createSut();
+
+    const response = await request(app).post('/login').send({
+      email,
+      password,
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('token');
+    expect(response.body).toHaveProperty('user');
+  });
+});
