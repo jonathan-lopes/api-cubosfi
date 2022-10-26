119f3708a7a824073e0cd0f9bdb18210b369043d
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-10-25 23:10:09 -0300

    style: eslint rule application

--- src/server.js
+++ src/server.js
@@ -3,15 +3,16 @@ require('dotenv').config({
 });
 require('express-async-errors');
 const express = require('express');
-const routes = require('./routes/router');
 const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
 const helmet = require('helmet');
+const routes = require('./routes/router');
 const errorMiddleware = require('./middleware/error');
 
 const app = express();
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(require('../swagger.json')));
+
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
