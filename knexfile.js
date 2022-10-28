7f24c83ce9e0e00ce7da48423bb53f00bb0e4876
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-10-27 23:52:22 -0300

    fix: change of développement for testing and checking the environment variable

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,12 @@
-require('dotenv').config();
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  development: {
+  test: {
     client: process.env.DB_CLIENT,
     connection: {
       filename: './test/database.sqlite',
