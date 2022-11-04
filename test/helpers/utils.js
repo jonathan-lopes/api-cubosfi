263cad48ce2a886dd3495f90b70806d903493e77
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-11-04 02:21:43 -0300

    feat: add a sut class family

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const knex = require('../../src/database/connection');
 
-class Sut {
+class SutUser {
   #name = '';
   #email = '';
   #password = '';
@@ -31,4 +31,76 @@ class Sut {
   }
 }
 
-module.exports = Sut;
+class SutCustomer {
+  #name;
+  #email;
+  #cpf;
+  #phone;
+
+  constructor(name, email, cpf, phone) {
+    this.#name = name;
+    this.#email = email;
+    this.#cpf = cpf;
+    this.#phone = phone;
+  }
+
+  async create() {
+    const [data] = await knex('customers')
+      .insert({
+        name: this.#name,
+        email: this.#email,
+        cpf: this.#cpf,
+        phone: this.#phone,
+      })
+      .returning(['id', 'name', 'email', 'cpf', 'phone', 'address_id']);
+
+    return data;
+  }
+
+  async clear() {
+    await knex('users').del().where({ email: this.#email });
+  }
+}
+
+class SutBilling {
+  #customer_id;
+  #description;
+  #status;
+  #value;
+  #due;
+
+  constructor(customer_id, description, status, value, due) {
+    this.#customer_id = customer_id;
+    this.#description = description;
+    this.#status = status;
+    this.#value = value;
+    this.#due = due;
+  }
+
+  async create() {
+    const [data] = await knex('billings')
+      .insert({
+        customer_id: this.#customer_id,
+        description: this.#description,
+        status: this.#status,
+        value: this.#value,
+        due: this.#due,
+      })
+      .returning([
+        'id',
+        'description',
+        'status',
+        'value',
+        'due',
+        'customer_id',
+      ]);
+
+    return data;
+  }
+
+  async clear() {
+    await knex('billings').del().where({ customer_id: this.#customer_id });
+  }
+}
+
+module.exports = { SutCustomer, SutUser, SutBilling };
