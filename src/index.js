202632b9ec963a8726e42ad895f37da72d00de72
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-11-17 02:23:17 -0300

    feat: use scheduled jobs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const app = require('./server');
+require('./jobs/deleteExpiredRefreshToken');
+require('./jobs/verifyBillingsOverdue');
 
 app.listen(process.env.PORT, () =>
   console.log(`🔥 server running at http://localhost:${process.env.PORT}`),
