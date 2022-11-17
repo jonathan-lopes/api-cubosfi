3cc8fc5549178ce83b672cd96e7ceee887b7e861
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-11-17 02:22:06 -0300

    feat: add job to check bills overdue

---
+++ src/jobs/verifyBillingsOverdue.js
@@ -0,0 +1,24 @@
+const schedule = require('node-schedule');
+const knex = require('../database/connection');
+
+const job = schedule.scheduleJob(
+  'verifyBillingsOverdue',
+  '0 18 * * 1-5',
+  async () => {
+    const bills = await knex('billings')
+      .where('due', '<', knex.fn.now())
+      .andWhere('status', 'pending');
+
+    const billsOverdue = [];
+
+    bills.forEach(async (bill) => {
+      billsOverdue.push(
+        knex('billings').update({ is_overdue: true }).where({ id: bill.id }),
+      );
+    });
+
+    await Promise.all(billsOverdue);
+  },
+);
+
+module.exports = job;
