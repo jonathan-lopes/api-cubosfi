877f82ffc60b611cedad774d9f99a9bc78f983af
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-11-22 00:17:15 -0300

    refactor: add the type, status and dateTime fields to the error middleware

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,9 +1,13 @@
 const errorMiddleware = (err, req, res, next) => {
-  const statusCode = err.statusCode ?? 500;
+  let statusCode = err.statusCode ?? 500;
   const message = err.statusCode ? err.message : 'Internal Server Error';
+  statusCode = err.name === 'ValidationError' ? 400 : statusCode;
 
-  return res.status(err.name === 'ValidationError' ? 400 : statusCode).json({
+  return res.status(statusCode).json({
+    type: err.constructor.name,
     message: err.name === 'ValidationError' ? err.message : message,
+    status: statusCode,
+    dateTime: new Date(),
   });
 };
 
