a77b657e0cc0980b6642e4261c3489abad08b6c9
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-11-22 00:04:57 -0300

    refactor: more validations in customer schema

--- src/validations/schemaCustomer.js
+++ src/validations/schemaCustomer.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-template-curly-in-string */
 const yup = require('./yupSettings');
 const { validateCPF, validadePhone } = require('../helpers/regex');
 
 const schemaCustomer = yup.object().shape({
-  name: yup.string().required(),
+  name: yup.string().strict().required(),
   email: yup.string().email('Formato de e-mail inválido').required(),
   cpf: yup
     .string()
@@ -14,12 +15,43 @@ const schemaCustomer = yup.object().shape({
     .strict()
     .matches(validadePhone, 'Fomato de telefone inválido')
     .required(),
-  cep: yup.string(),
-  address: yup.string(),
-  complement: yup.string(),
-  district: yup.string(),
-  city: yup.string(),
-  uf: yup.string(),
+  address: yup
+    .object({
+      cep: yup.string().strict().max(9),
+      street: yup.string().strict(),
+      complement: yup.string().strict(),
+      district: yup.string().strict(),
+      city: yup.string().strict(),
+      uf: yup
+        .string()
+        .strict()
+        .matches(/^[A-Z]{2}$/gm, {
+          excludeEmptyString: true,
+          message: 'address.uf deve ter exatamente 2 caracteres em maiúsculo',
+        }),
+    })
+    .test(
+      'is-empty',
+      '${path} object is empty',
+      (value) => Object.keys(value).length,
+    )
+    .test(
+      'is-aaa',
+      '${path} com propriedades não suportadas',
+      (value, context) => {
+        const keysOriginalValue = Object.keys(value);
+        // eslint-disable-next-line no-underscore-dangle
+        const nodes = context.schema._nodes;
+
+        for (let i = 0; i < keysOriginalValue.length; i++) {
+          if (!nodes.includes(keysOriginalValue[i])) {
+            return false;
+          }
+        }
+
+        return true;
+      },
+    ),
 });
 
 module.exports = schemaCustomer;
