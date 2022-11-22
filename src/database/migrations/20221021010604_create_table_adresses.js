5fd004966d0ba47ad9661afd433874e49889c857
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-11-22 00:10:53 -0300

    refactor: change the address property name to street and add created_at and updated_at fields

--- src/database/migrations/20221021010604_create_table_adresses.js
+++ src/database/migrations/20221021010604_create_table_adresses.js
@@ -8,12 +8,14 @@ exports.up = (knex) =>
   knex.schema
     .createTable('adresses', (table) => {
       table.increments('id');
-      table.text('address');
+      table.text('street');
       table.text('complement');
       table.string('cep', 9);
       table.text('district');
       table.text('city');
       table.string('uf', 2);
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
     .then(() => knex.raw(onUpdateTrigger('adresses')));
 
