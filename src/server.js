dff141cef1f420bd86fc2fb367cc40b32e62ef81
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-11-24 01:52:12 -0300

    refactor: change documentation route to /api/docs

--- src/server.js
+++ src/server.js
@@ -11,7 +11,11 @@ const errorMiddleware = require('./middlewares/error');
 
 const app = express();
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(require('../swagger.json')));
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(require('../swagger.json')),
+);
 
 app.use(express.json());
 app.use(cors());
