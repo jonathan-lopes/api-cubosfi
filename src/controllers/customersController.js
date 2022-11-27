68eed1471fa657dc00cd7cadf3dd1607130afc3f
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-11-27 00:26:25 -0300

    perf: remove unnecessary loops, and eliminate unnecessary code snippets

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -1,6 +1,5 @@
 const knex = require('../database/connection');
 const schemaCustomer = require('../validations/schemaCustomer');
-const paymentStatus = require('../helpers/paymentStatus');
 const {
   ConflictError,
   DatabaseError,
@@ -9,7 +8,6 @@ const {
 
 const create = async (req, res) => {
   const { name, email, cpf, phone, address } = req.body;
-  let addressId = 0;
 
   await schemaCustomer.validate({
     name,
@@ -38,20 +36,23 @@ const create = async (req, res) => {
     phone,
   };
 
-  if (address) {
-    [addressId] = await knex('adresses').insert(address).returning('id');
+  const [insertedCustomer] = await knex('customers')
+    .insert(body)
+    .returning('id');
 
-    if (!addressId) {
-      throw new DatabaseError('Não foi possível cadastrar o cliente');
-    }
-
-    body.address_id = addressId.id;
+  if (insertedCustomer === 0) {
+    throw new DatabaseError('Não foi possível cadastrar o cliente');
   }
 
-  const insertCustomer = await knex('customers').insert(body);
+  if (address) {
+    const insertedAddress = await knex('adresses').insert({
+      ...address,
+      customer_id: insertedCustomer.id,
+    });
 
-  if (insertCustomer === 0) {
-    throw new DatabaseError('Não foi possível cadastrar o cliente');
+    if (!insertedAddress) {
+      throw new DatabaseError('Não foi possível cadastrar o cliente');
+    }
   }
 
   return res.status(201).json();
@@ -65,7 +66,6 @@ const getAll = async (req, res) => {
     'email',
     'cpf',
     'phone',
-    'address_id',
   );
 
   const billsData = await knex('billings').select(
@@ -74,12 +74,6 @@ const getAll = async (req, res) => {
     'due',
     'customer_id',
   );
-
-  for (const bill of billsData) {
-    if (paymentStatus.isOverdue(bill.due, bill.status)) {
-      bill.status = 'overdue';
-    }
-  }
 
   for (let customer of customers) {
     const bills = billsData.filter((bill) => bill.customer_id === customer.id);
@@ -95,7 +89,7 @@ const getOne = async (req, res) => {
   const { id } = req.params;
 
   const customer = await knex('customers')
-    .select('id', 'name', 'email', 'cpf', 'phone', 'address_id')
+    .select('id', 'name', 'email', 'cpf', 'phone')
     .where({ id })
     .first();
 
@@ -104,8 +98,16 @@ const getOne = async (req, res) => {
   }
 
   const address = await knex('adresses')
-    .select('street', 'complement', 'cep', 'district', 'city', 'uf')
-    .where({ id: customer.address_id })
+    .select(
+      'street',
+      'complement',
+      'cep',
+      'district',
+      'city',
+      'uf',
+      'customer_id',
+    )
+    .where({ customer_id: customer.id })
     .first();
 
   const bills = await knex('billings')
@@ -152,48 +154,33 @@ const update = async (req, res) => {
     phone,
   };
 
-  const { address_id: userWithAddress } = await knex('customers')
-    .select('address_id')
-    .where({ id })
-    .first();
+  const updatedCustomer = await knex('customers').update(body).where({ id });
 
-  if (userWithAddress) {
-    const [customerAddressIdReturning] = await knex('customers')
-      .update(body)
-      .where({ id })
-      .returning('address_id');
-
-    if (customerAddressIdReturning === 0) {
-      throw new DatabaseError('Não foi possível cadastrar o cliente');
-    }
-
-    if (address) {
-      const updateAddress = await knex('adresses')
-        .update(address)
-        .where({ id: customerAddressIdReturning.address_id });
-
-      if (updateAddress === 0) {
-        throw new DatabaseError('Não foi possível cadastrar o cliente');
-      }
-    }
-
-    return res.status(204).json();
+  if (updatedCustomer === 0) {
+    throw new DatabaseError('Não foi possível cadastrar o cliente');
   }
 
-  if (address) {
-    const [insertAddress] = await knex('adresses')
-      .insert(address)
-      .returning('id');
+  const customerWithAddress = await knex('adresses')
+    .where({ customer_id: id })
+    .first();
 
-    if (insertAddress === 0) {
+  if (customerWithAddress && address) {
+    const updateAddress = await knex('adresses')
+      .update(address)
+      .where({ customer_id: id });
+
+    if (updateAddress === 0) {
       throw new DatabaseError('Não foi possível cadastrar o cliente');
     }
+  }
 
-    const customerAddressIdReturning = await knex('customers')
-      .update({ ...body, address_id: insertAddress.id })
-      .where({ id });
+  if (!customerWithAddress && address) {
+    const insertedAddress = await knex('adresses').insert({
+      ...address,
+      customer_id: id,
+    });
 
-    if (customerAddressIdReturning === 0) {
+    if (insertedAddress === 0) {
       throw new DatabaseError('Não foi possível cadastrar o cliente');
     }
   }
