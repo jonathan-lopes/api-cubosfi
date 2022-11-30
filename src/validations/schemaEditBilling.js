6dd7285657cfe89dadc05c319b235094f8d2b265
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-11-30 02:40:17 -0300

    refactor: add more validations to billing schemes

--- src/validations/schemaEditBilling.js
+++ src/validations/schemaEditBilling.js
@@ -2,13 +2,13 @@ const { validateStatus } = require('../helpers/regex');
 const yup = require('./yupSettings');
 
 const editBillingSchema = yup.object().shape({
-  description: yup.string().required(),
+  description: yup.string().max(128).required(),
   status: yup
     .string()
     .matches(validateStatus, 'status devem ser paid ou pending')
     .required(),
-  value: yup.number().integer().required(),
-  due: yup.date().required(),
+  value: yup.number().strict().integer().positive().required(),
+  due: yup.date().min('1900-01-01').required(),
 });
 
 module.exports = editBillingSchema;
