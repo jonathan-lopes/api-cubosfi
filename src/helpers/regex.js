88cbdde21b3803ea491d51fd8295b0d181d4dbd2
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-12-05 22:09:59 -0300

    refactor: changes the name of the validateCPF regex to validateCPFCNPJ and shortens the regex

--- src/helpers/regex.js
+++ src/helpers/regex.js
@@ -1,10 +1,10 @@
 const validadePhone = /^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}-?[0-9]{4}$/gm;
-const validateCPF =
-  /([0-9]{2}.?[0-9]{3}.?[0-9]{3}[/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}.?[0-9]{3}.?[0-9]{3}[-]?[0-9]{2})/gm;
+const validateCPFCNPJ =
+  /(\d{2}.?\d{3}.?\d{3}\/?\d{4}-?\d{2})|(\d{3}.?){2}\d{3}-?\d{2}/gm;
 const validateStatus = /^p(aid|ending)$/gm;
 
 module.exports = {
   validadePhone,
-  validateCPF,
+  validateCPFCNPJ,
   validateStatus,
 };
