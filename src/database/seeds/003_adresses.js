e29a7fb9bdeaf4e34d3978f78534c02883d38376
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-12-06 21:48:43 -0300

    refactor: change seeds to uuid

--- src/database/seeds/003_adresses.js
+++ src/database/seeds/003_adresses.js
@@ -1,3 +1,6 @@
+const { v4: uuidv4 } = require('uuid');
+const { idsCustomers } = require('./002_customers');
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
@@ -6,20 +9,24 @@ exports.seed = async (knex) => {
   await knex('adresses').del();
   await knex('adresses').insert([
     {
+      id: uuidv4(),
       cep: '04002-010',
-      address: 'Rua Treze de Maio',
+      street: 'Rua Treze de Maio',
       complement: 'Apartamento',
       district: 'Paraíso',
       city: 'São Paulo',
       uf: 'SP',
+      customer_id: idsCustomers.customer1,
     },
     {
+      id: uuidv4(),
       cep: '40170-110',
-      address: ' Avenida Adhemar de Barros ',
+      street: ' Avenida Adhemar de Barros ',
       complement: 'Ondina',
       district: 'Ondina',
       city: 'Salvador',
       uf: 'BA',
+      customer_id: idsCustomers.customer2,
     },
   ]);
 };
--- src/database/seeds/004_billings.js
+++ src/database/seeds/004_billings.js
@@ -1,3 +1,5 @@
+const { v4: uuidv4 } = require('uuid');
+const { idsCustomers } = require('./002_customers');
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
@@ -6,44 +8,49 @@ exports.seed = async (knex) => {
   await knex('billings').del();
   await knex('billings').insert([
     {
+      id: uuidv4(),
       description:
         'commodo ipsum. Suspendisse non leo. Vivamus nibh dolor, nonummy ac,',
       status: 'paid',
       value: 40000,
       due: '2023-03-10',
-      customer_id: 1,
+      customer_id: idsCustomers.customer1,
     },
     {
+      id: uuidv4(),
       description:
         'molestie tortor nibh sit amet orci. Ut sagittis lobortis mauris.',
       status: 'paid',
       value: 40350,
       due: '2021-06-08',
-      customer_id: 1,
+      customer_id: idsCustomers.customer1,
     },
     {
+      id: uuidv4(),
       description:
         'risus. Donec nibh enim, gravida sit amet, dapibus id, blandit',
       status: 'pending',
       value: 95600,
       due: '2022-03-18',
-      customer_id: 2,
+      customer_id: idsCustomers.customer2,
     },
     {
+      id: uuidv4(),
       description:
         'et malesuada fames ac turpis egestas. Fusce aliquet magna a',
       status: 'pending',
       value: 75600,
       due: '2021-04-29',
-      customer_id: 2,
+      customer_id: idsCustomers.customer2,
     },
     {
+      id: uuidv4(),
       description:
         'Curae Donec tincidunt. Donec vitae erat vel pede blandit congue.',
       status: 'pending',
       value: 25500,
       due: '2022-03-11',
-      customer_id: 2,
+      customer_id: idsCustomers.customer2,
     },
   ]);
 };
