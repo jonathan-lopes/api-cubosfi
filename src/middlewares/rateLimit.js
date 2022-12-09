99eb60ae6bf53cebe1dc8a76a1a447a8a38c1f33
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-12-09 01:00:23 -0300

    feat: add rate limit

---
+++ src/middlewares/rateLimit.js
@@ -0,0 +1,24 @@
+const rateLimit = require('express-rate-limit');
+const slowDown = require('express-slow-down');
+const { requests } = require('../config');
+
+const limiter = rateLimit({
+  windowMs: requests.rateLimit.window,
+  max: requests.rateLimit.max,
+  handler: (req, res) => {
+    res.status(429).json({
+      type: 'manyRequestsError',
+      message: 'Muitos pedidos. Por favor tente mais tarde.',
+      status: 429,
+      dateTime: new Date(),
+    });
+  },
+});
+
+const slower = slowDown({
+  windowMs: requests.slowDown.window,
+  delayAfter: requests.slowDown.delayAfter,
+  delayMs: requests.slowDown.delayMs,
+});
+
+module.exports = [slower, limiter];
