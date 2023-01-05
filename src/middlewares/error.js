cf35d6ae39705ed8c564ed26832c0ade7b219f0e
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2023-01-04 23:07:12 -0300

    refactor: refactoring for middleware to return an object containing all errors

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -4,18 +4,46 @@ const errorMiddleware = (err, req, res, next) => {
   let statusCode = err.statusCode ?? 500;
   let message = err.statusCode ? err.message : 'Internal Server Error';
 
-  statusCode = err.name === 'ValidationError' ? 400 : statusCode;
-  message = err.name === 'ValidationError' ? err.message : message;
+  const validationErrors = {};
+  const queryErrors = {};
+  if (err.name === 'ValidationError') {
+    statusCode = 400;
+    message = err.inner;
+
+    message.forEach((error) => {
+      if (!error.path) return;
+
+      const haveQueryError = error.path.includes('query');
+
+      if (haveQueryError) {
+        const querypath = error.path.split('.')[1];
+        queryErrors[querypath] = error.message;
+
+        validationErrors.query = queryErrors;
+      }
+
+      if (!haveQueryError) {
+        validationErrors[error.path] = error.message;
+      }
+    });
+  }
+
+  const apiErrors = Object.keys(validationErrors).length
+    ? validationErrors
+    : { error: message };
+
+  const logErrorDescription =
+    statusCode === 500 ? err.message : Object.values(apiErrors);
 
   logger.error({
     message: `${req.ip} ${req.get('User-Agent')} ${req.method} ${req.path} ${
       req.user?.id || '-'
-    } ${err.constructor.name} ${err.message} ${statusCode}`,
+    } ${err.constructor.name} ${logErrorDescription} ${statusCode}`,
   });
 
   return res.status(statusCode).json({
     type: err.constructor.name,
-    message,
+    message: apiErrors,
     status: statusCode,
     dateTime: new Date(),
   });
