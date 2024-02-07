17fb8e4bdb899281ff8577315117a1b8f309751f
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2024-02-07 20:08:30 -0300

    chore: add the faker package

--- test/helpers/randomData.js
+++ test/helpers/randomData.js
@@ -2,7 +2,7 @@ const { faker } = require('@faker-js/faker/locale/pt_BR');
 
 const createRandomUser = () => {
   return {
-    name: faker.name.fullName(),
+    name: faker.person.fullName(),
     email: faker.internet.email(),
     password: faker.internet.password(),
   };
@@ -12,16 +12,16 @@ const createRandomBilling = (due, status = 'paid') => {
   return {
     description: faker.lorem.sentence(5),
     status,
-    value: +faker.random.numeric(5),
+    value: +faker.string.numeric(5),
     due,
   };
 };
 
 const createRandomCustomer = () => {
   return {
-    name: faker.name.fullName(),
+    name: faker.person.fullName(),
     email: faker.internet.email(),
-    cpf: faker.random.numeric(11, { allowLeadingZeros: true }),
+    cpf: faker.string.numeric(11, { allowLeadingZeros: true }),
     phone: faker.phone.number('##9########'),
   };
 };
