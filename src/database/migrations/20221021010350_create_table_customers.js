42ea1dee4b2773500e73409cc890d7b75599910b
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2024-02-07 19:43:25 -0300

    refactor: add foreign key address_id in customers table

--- src/database/migrations/20221021010350_create_table_customers.js
+++ src/database/migrations/20221021010350_create_table_customers.js
@@ -16,6 +16,12 @@ exports.up = (knex) =>
       table.string('email', 80).unique().notNullable();
       table.text('cpf').unique().notNullable();
       table.text('phone').notNullable();
+      table.uuid('address_id');
+      table
+        .foreign('address_id')
+        .references('id')
+        .inTable('adresses')
+        .onDelete('CASCADE');
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
