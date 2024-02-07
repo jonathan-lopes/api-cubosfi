412aa9567b00843dad21d1b30940f75fe2757fd4
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2024-02-07 00:24:31 -0300

    fix: remove use of uuid package
    package uuid was no longer used in the project

--- src/database/seeds/003_adresses.js
+++ src/database/seeds/003_adresses.js
@@ -1,6 +1,3 @@
-const { v4: uuidv4 } = require('uuid');
-const { idsCustomers } = require('./002_customers');
-
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
@@ -9,24 +6,20 @@ exports.seed = async (knex) => {
   await knex('adresses').del();
   await knex('adresses').insert([
     {
-      id: uuidv4(),
       cep: '04002-010',
       street: 'Rua Treze de Maio',
       complement: 'Apartamento',
       district: 'Paraíso',
       city: 'São Paulo',
       uf: 'SP',
-      customer_id: idsCustomers.customer1,
     },
     {
-      id: uuidv4(),
       cep: '40170-110',
-      street: ' Avenida Adhemar de Barros ',
+      street: 'Avenida Adhemar de Barros',
       complement: 'Ondina',
       district: 'Ondina',
       city: 'Salvador',
       uf: 'BA',
-      customer_id: idsCustomers.customer2,
     },
   ]);
 };
--- src/database/seeds/004_billings.js
+++ src/database/seeds/004_billings.js
@@ -1,56 +1,51 @@
-const { v4: uuidv4 } = require('uuid');
-const { idsCustomers } = require('./002_customers');
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.seed = async (knex) => {
+  const customersIDs = await knex('customers').select('id');
+
   await knex('billings').del();
   await knex('billings').insert([
     {
-      id: uuidv4(),
       description:
         'commodo ipsum. Suspendisse non leo. Vivamus nibh dolor, nonummy ac,',
       status: 'paid',
       value: 40000,
       due: '2023-03-10',
-      customer_id: idsCustomers.customer1,
+      customer_id: customersIDs[0].id,
     },
     {
-      id: uuidv4(),
       description:
         'molestie tortor nibh sit amet orci. Ut sagittis lobortis mauris.',
       status: 'paid',
       value: 40350,
       due: '2021-06-08',
-      customer_id: idsCustomers.customer1,
+      customer_id: customersIDs[0].id,
     },
     {
-      id: uuidv4(),
       description:
         'risus. Donec nibh enim, gravida sit amet, dapibus id, blandit',
       status: 'pending',
       value: 95600,
       due: '2022-03-18',
-      customer_id: idsCustomers.customer2,
+      customer_id: customersIDs[1].id,
     },
     {
-      id: uuidv4(),
       description:
         'et malesuada fames ac turpis egestas. Fusce aliquet magna a',
       status: 'pending',
       value: 75600,
       due: '2021-04-29',
-      customer_id: idsCustomers.customer2,
+      customer_id: customersIDs[1].id,
     },
     {
-      id: uuidv4(),
       description:
         'Curae Donec tincidunt. Donec vitae erat vel pede blandit congue.',
       status: 'pending',
       value: 25500,
       due: '2022-03-11',
-      customer_id: idsCustomers.customer2,
+      customer_id: customersIDs[2].id,
     },
   ]);
 };
