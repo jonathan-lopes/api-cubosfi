fed64e7f83b5f15e7d32e7be53a3832df59b5806
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2024-02-07 19:31:46 -0300

    test: segments billing tests for better readability

---
+++ test/billings/create.spec.js
@@ -0,0 +1,68 @@
+const request = require('supertest');
+const app = require('../../src/server');
+const login = require('../helpers/login');
+const {
+  createRandomUser,
+  createRandomBilling,
+  createRandomCustomer,
+} = require('../helpers/randomData');
+const { SutBilling, SutCustomer } = require('../helpers/utils');
+
+let token = '';
+
+describe('Create Billing', () => {
+  beforeAll(async () => {
+    token = await login(app, createRandomUser(), 'login');
+  });
+
+  it('should return status 400 if no customer_id, description, status, value, and due are sent in the request body', async () => {
+    const response = await request(app)
+      .post('/billings')
+      .send({})
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty('status', 400);
+    expect(response.body).toHaveProperty('type', 'ValidationError');
+    expect(response.body).toHaveProperty('dateTime');
+    expect(response.body).toHaveProperty('message.due', 'due é obrigatório');
+    expect(response.body).toHaveProperty(
+      'message.customer_id',
+      'customer_id é obrigatório',
+    );
+    expect(response.body).toHaveProperty(
+      'message.description',
+      'description é obrigatório',
+    );
+    expect(response.body).toHaveProperty(
+      'message.status',
+      'status é obrigatório',
+    );
+    expect(response.body).toHaveProperty(
+      'message.value',
+      'value é obrigatório',
+    );
+  });
+
+  it('should return status 201 if a billing has been registered', async () => {
+    const { id: customerID } = await new SutCustomer(
+      createRandomCustomer(),
+    ).create();
+
+    const bill = createRandomBilling('2022-10-15');
+
+    const sutBilling = new SutBilling({
+      customer_id: customerID,
+      ...bill,
+    });
+
+    const dataBilling = await sutBilling.create();
+
+    const response = await request(app)
+      .post('/billings')
+      .send(dataBilling)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(201);
+  });
+});
---
+++ test/billings/getOne.spec.js
@@ -0,0 +1,58 @@
+const request = require('supertest');
+const app = require('../../src/server');
+const login = require('../helpers/login');
+const {
+  createRandomUser,
+  createRandomBilling,
+  createRandomCustomer,
+} = require('../helpers/randomData');
+const { SutBilling, SutCustomer } = require('../helpers/utils');
+
+let token = '';
+
+describe('Get One Billing', () => {
+  beforeAll(async () => {
+    token = await login(app, createRandomUser(), 'login');
+  });
+
+  it('should return status 404 if billing does not exist', async () => {
+    const response = await request(app)
+      .get('/billings/1c53ae80-5042-4db9-aad5-3e2d510c98b3')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toHaveProperty('status', 404);
+    expect(response.body).toHaveProperty('type', 'NotFoundError');
+    expect(response.body).toHaveProperty('dateTime');
+    expect(response.body).toHaveProperty(
+      'message.error',
+      'Cobrança não encontrada',
+    );
+  });
+
+  it('should return billing', async () => {
+    const { id: customerID } = await new SutCustomer(
+      createRandomCustomer(),
+    ).create();
+
+    const randomBill = createRandomBilling('2022-10-15');
+
+    const sutBilling = new SutBilling({
+      customer_id: customerID,
+      ...randomBill,
+    });
+
+    const dataBilling = await sutBilling.create();
+
+    await request(app)
+      .post('/billings')
+      .send(dataBilling)
+      .set('Authorization', `Bearer ${token}`);
+
+    const response = await request(app)
+      .get(`/billings/${dataBilling.id}`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(200);
+  });
+});
