796cdb43750a1eb77198b40510d92739562ab134
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2024-03-02 03:02:59 -0300

    test: add test for the value query parameter of the billings route

---
+++ test/billings/queryParams/value.spec.js
@@ -0,0 +1,60 @@
+const request = require('supertest');
+const app = require('../../../src/server');
+const login = require('../../helpers/login');
+const { SutCustomer, SutBilling } = require('../../helpers/utils');
+const {
+  createRandomCustomer,
+  createRandomBilling,
+  createRandomUser,
+} = require('../../helpers/randomData');
+const knex = require('../../../src/database');
+
+let token = '';
+
+describe('List Billings Based on "value" Query Parameter', () => {
+  beforeAll(async () => {
+    token = await login(app, createRandomUser());
+  });
+
+  afterAll(async () => await knex.destroy());
+
+  it('should be possible to filter billings by the specified amount', async () => {
+    const customer = new SutCustomer(createRandomCustomer());
+    const { id } = await customer.create();
+
+    const value = 10000;
+
+    const randomBill = createRandomBilling('2023-12-04');
+
+    randomBill.value = value;
+
+    await new SutBilling({
+      customer_id: id,
+      ...randomBill,
+    }).create();
+
+    const response = await request(app)
+      .get('/billings')
+      .query({ value: value })
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body[0]).toHaveProperty('value', value);
+  });
+
+  it('should fail if the value passed is not a positive integer', async () => {
+    const response = await request(app)
+      .get('/billings')
+      .query({ value: 460.48 })
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty('dateTime');
+    expect(response.body).toHaveProperty('status', 400);
+    expect(response.body).toHaveProperty('type', 'ValidationError');
+    expect(response.body).toHaveProperty(
+      'message.query.value',
+      'query.value deve ser um número inteiro',
+    );
+  });
+});
