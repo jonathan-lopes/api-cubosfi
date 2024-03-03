97e56fcfb7ce704b70905b782ba2e38ca5f99035
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2024-03-02 21:27:50 -0300

    test: replace the tuBe function with boolean context functions

--- test/isValidUUID.spec.js
+++ test/isValidUUID.spec.js
@@ -2,12 +2,12 @@ const isValidUUID = require('../src/helpers/isValidUUID');
 
 describe('is Valid UUID', () => {
   it('should return true if UUID is valid', () => {
-    expect(isValidUUID('5a6564c7-cd5d-48c5-ac84-93d4e762463f')).toBe(true);
+    expect(isValidUUID('5a6564c7-cd5d-48c5-ac84-93d4e762463f')).toBeTruthy();
   });
 
   it('should return false if UUID is invalid', () => {
     expect(
       isValidUUID('31545f6b4c3-9624-4a48-a3b9-f335e07fcb15646746878'),
-    ).toBe(false);
+    ).toBeFalsy();
   });
 });
