237164138b0ccecea51e126aae3fd93eb9fb3ae0
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2024-03-06 02:29:16 -0300

    test: remove test for unsupported properties on address object

--- src/validations/schemaCustomer.js
+++ src/validations/schemaCustomer.js
@@ -19,41 +19,23 @@ const schemaCustomer = yup.object().shape({
     .strict()
     .matches(validatePhone, 'Formato de telefone inválido')
     .required(),
-  address: yup
-    .object({
-      cep: yup
-        .string()
-        .strict()
-        .matches(validateCEP, 'Formato de address.cep inválido'),
-      street: yup.string().strict(),
-      complement: yup.string().strict(),
-      district: yup.string().strict(),
-      city: yup.string().strict(),
-      uf: yup
-        .string()
-        .strict()
-        .matches(/^[A-Z]{2}$/gm, {
-          excludeEmptyString: true,
-          message: 'address.uf deve ter exatamente 2 caracteres em maiúsculo',
-        }),
-    })
-    .test(
-      'is-unknown',
-      '${path} com propriedades não suportadas',
-      (value, context) => {
-        const keysOriginalValue = Object.keys(value);
-        // eslint-disable-next-line no-underscore-dangle
-        const nodes = context.schema._nodes;
-
-        for (let i = 0; i < keysOriginalValue.length; i++) {
-          if (!nodes.includes(keysOriginalValue[i])) {
-            return false;
-          }
-        }
-
-        return true;
-      },
-    ),
+  address: yup.object({
+    cep: yup
+      .string()
+      .strict()
+      .matches(validateCEP, 'Formato de address.cep inválido'),
+    street: yup.string().strict(),
+    complement: yup.string().strict(),
+    district: yup.string().strict(),
+    city: yup.string().strict(),
+    uf: yup
+      .string()
+      .strict()
+      .matches(/^[A-Z]{2}$/gm, {
+        excludeEmptyString: true,
+        message: 'address.uf deve ter exatamente 2 caracteres em maiúsculo',
+      }),
+  }),
 });
 
 module.exports = schemaCustomer;
