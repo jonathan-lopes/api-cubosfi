676c5a5cc438549d212bdc63d30e1a5c6ae1bc56
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2024-03-06 02:02:14 -0300

    test: add test to validate the uuid to return a client

--- test/customers/getOne.spec.js
+++ test/customers/getOne.spec.js
@@ -15,7 +15,7 @@ describe('Get One Customer', () => {
 
   afterAll(async () => await knex.destroy());
 
-  it('should return status 400 if a customer does not exist', async () => {
+  it('should return status 404 if a customer does not exist', async () => {
     const response = await request(app)
       .get('/customers/a334d00d-07bc-4c38-a909-bae7328e60e9')
       .set('Authorization', `Bearer ${token}`);
@@ -28,6 +28,21 @@ describe('Get One Customer', () => {
     expect(response.body).toHaveProperty('status', 404);
     expect(response.body).toHaveProperty('type', 'NotFoundError');
     expect(response.body).toHaveProperty('dateTime');
+  });
+
+  it('should return status 400 if the uuid is invalid', async () => {
+    const response = await request(app)
+      .get('/customers/1')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty('status', 400);
+    expect(response.body).toHaveProperty('type', 'BadRequestError');
+    expect(response.body).toHaveProperty('dateTime');
+    expect(response.body).toHaveProperty(
+      'message.error',
+      'Id de cliente inválido',
+    );
   });
 
   it('should return a customer', async () => {
