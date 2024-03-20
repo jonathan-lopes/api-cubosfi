25a520908dd20f19f60c2d85c3a0ae1c0ffbd0c2
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2024-03-20 02:14:23 -0300

    test: add test for page_size query parameter

---
+++ test/customers/queryParams/pageSize.spec.js
@@ -0,0 +1,60 @@
+const login = require('../../helpers/login');
+const request = require('supertest');
+const app = require('../../../src/server');
+const {
+  createRandomUser,
+  createRandomCustomer,
+} = require('../../helpers/randomData');
+const knex = require('../../../src/database');
+const { SutCustomer } = require('../../helpers/utils');
+
+let token = '';
+
+describe('List Customers Based on "page_size" Query Parameter', () => {
+  beforeAll(async () => {
+    token = await login(app, createRandomUser());
+  });
+
+  afterAll(async () => await knex.destroy());
+
+  it('should return a maximum of 10 clients if page_size is not sent in the query parameter', async () => {
+    await new SutCustomer(createRandomCustomer()).create();
+
+    const response = await request(app)
+      .get('/customers')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBeLessThanOrEqual(10);
+  });
+
+  it('should return customers with a page_size of 2', async () => {
+    await new SutCustomer(createRandomCustomer()).create();
+
+    const page_size = 2;
+
+    const response = await request(app)
+      .get('/customers')
+      .query({ page_size })
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBeLessThanOrEqual(page_size);
+  });
+
+  it('should fail if page_size is not a positive integer', async () => {
+    const response = await request(app)
+      .get('/customers')
+      .query({ page_size: 'foo' })
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty('status', 400);
+    expect(response.body).toHaveProperty('type', 'ValidationError');
+    expect(response.body).toHaveProperty('dateTime');
+    expect(response.body).toHaveProperty(
+      'message.query.page_size',
+      'page_size deve ser do tipo number',
+    );
+  });
+});
