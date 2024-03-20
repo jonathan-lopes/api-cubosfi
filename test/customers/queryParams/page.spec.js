be2875b0f697f1a7f9d35f8ddb1eb210620d036c
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2024-03-20 02:11:31 -0300

    test: add test for page query parameter

---
+++ test/customers/queryParams/page.spec.js
@@ -0,0 +1,53 @@
+const login = require('../../helpers/login');
+const request = require('supertest');
+const app = require('../../../src/server');
+const { createRandomUser } = require('../../helpers/randomData');
+const knex = require('../../../src/database');
+
+let token = '';
+
+describe('List Customers Based on "page" Query Parameter', () => {
+  beforeAll(async () => {
+    token = await login(app, createRandomUser());
+  });
+
+  afterAll(async () => await knex.destroy());
+
+  it('should return the first customer page if page is not sent in the query parameter', async () => {
+    const response = await request(app)
+      .get('/customers')
+      .set('Authorization', `Bearer ${token}`);
+
+    const pagination = JSON.parse(response.get('x-pagination'));
+
+    expect(response.statusCode).toBe(200);
+    expect(pagination.current_page).toBe(1);
+  });
+
+  it('should return customers on page 1', async () => {
+    const response = await request(app)
+      .get('/customers')
+      .set('Authorization', `Bearer ${token}`);
+
+    const pagination = JSON.parse(response.get('x-pagination'));
+
+    expect(response.statusCode).toBe(200);
+    expect(pagination.current_page).toBe(1);
+  });
+
+  it('should fail if page is not a positive integer', async () => {
+    const response = await request(app)
+      .get('/customers')
+      .query({ page: 'foo' })
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty('status', 400);
+    expect(response.body).toHaveProperty('type', 'ValidationError');
+    expect(response.body).toHaveProperty('dateTime');
+    expect(response.body).toHaveProperty(
+      'message.query.page',
+      'page deve ser do tipo number',
+    );
+  });
+});
